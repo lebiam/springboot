3551cd95419254fe4a96dc3398ce15e0dceca4d6
Antoine Biamouret <a.biamouret@cargo-services.fr>
2020-06-26 11:25:34 +0200

    Add logger

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -8,12 +8,15 @@ import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.*;
@@ -22,11 +25,18 @@ import java.util.*;
 @RestController
 public class ProductController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
     @Autowired
     private ProductDao productDao;
 
+    @Autowired
+    private HttpServletRequest requestContext;
+
+
     @GetMapping(value="/Produits")
     public MappingJacksonValue listeProduits() {
+        logger.info("Début d'appel au service Produit de la requête: "+requestContext.getHeader("req-id"));
         List<Product> products = productDao.findAll();
 
         SimpleBeanPropertyFilter myFilter = SimpleBeanPropertyFilter.serializeAllExcept("prixAchat");
@@ -43,6 +53,7 @@ public class ProductController {
     @ApiOperation(value = "Récupère un produit grâce à son ID à condition que celui-ci soit en stock!")
     @GetMapping(value="/Produits/{id}")
     public Product afficherUnProduit(@PathVariable int id) throws ProduitIntrouvableException {
+        logger.info("Début d'appel au service Produit de la requête: "+ requestContext.getHeader("req-id"));
         Product product = productDao.findById(id);
         if(product == null) throw new ProduitIntrouvableException("Le produit avec l'id " + id +" est introuvable.");
         return product;
@@ -51,6 +62,7 @@ public class ProductController {
     @ApiOperation(value = "Affiche la marge des produits")
     @GetMapping(value="/AdminProduits")
     public Map<String, Integer> calculerMargeProduit() {
+        logger.info("Début d'appel au service Produit de la requête: "+requestContext.getHeader("req-id"));
         Map<String, Integer> response = new HashMap<>();
         List<Product> products = productDao.findAll();
         products.forEach((e) -> {
@@ -62,6 +74,7 @@ public class ProductController {
     @ApiOperation(value = "Range de manière alphabétique les produits.")
     @GetMapping(value="/sortProductsByLetters")
     public List<Product> trierProduitsParOrdreAlphabetique() {
+        logger.info("Début d'appel au service Produit de la requête: "+requestContext.getHeader("req-id"));
         List<Product> products = productDao.findAll();
         List<Product> sortProductsByName = new ArrayList<>();
 
@@ -77,17 +90,20 @@ public class ProductController {
 
     @GetMapping(value = "test/produits/{prixLimit}")
     public List<Product> testeDeRequetesPrix(@PathVariable int prixLimit) {
+        logger.info("Début d'appel au service Produit de la requête: "+requestContext.getHeader("req-id"));
         return productDao.findByPrixGreaterThan(400);
     }
 
     @GetMapping(value = "test/produits/recherche/{recherche}")
     public List<Product> testeDeRequetesRecherche(@PathVariable String
                                                  recherche) {
+        logger.info("Début d'appel au service Produit de la requête: "+requestContext.getHeader("req-id"));
         return productDao.findByNomLike("%"+recherche+"%");
     }
 
     @PostMapping(value="/Produits")
     public ResponseEntity<Void> ajouterProduit(@Valid  @RequestBody Product product) throws ProduitGratuitException {
+        logger.info("Début d'appel au service Produit de la requête: "+requestContext.getHeader("req-id"));
         if(product.getPrix() == 0){
             throw new ProduitGratuitException("Tu ne pas pas vendre un produit gratuitement.");
         }
@@ -107,11 +123,13 @@ public class ProductController {
 
     @DeleteMapping (value = "/Produits/{id}")
     public void supprimerProduit(@PathVariable int id) {
+        logger.info("Début d'appel au service Produit de la requête: "+requestContext.getHeader("req-id"));
         productDao.deleteById(id);
     }
 
     @PutMapping (value = "/Produits")
     public void updateProduit(@RequestBody Product product) {
+        logger.info("Début d'appel au service Produit de la requête: "+requestContext.getHeader("req-id"));
         productDao.save(product);
     }
 }
